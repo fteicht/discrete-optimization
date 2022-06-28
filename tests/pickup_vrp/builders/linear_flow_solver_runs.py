83a22e552506bda11b05a094c255ef827f61c0a0
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-06-28 16:45:05 +0200

    [wip] Make work linear_flow_solver_runs

--- tests/pickup_vrp/builders/linear_flow_solver_runs.py
+++ tests/pickup_vrp/builders/linear_flow_solver_runs.py
@@ -24,7 +24,7 @@ else:
 @pytest.mark.skipif(not gurobi_available, reason="You need Gurobi to test this solver.")
 def test_tsp():
     files_available = tsp_parser.get_data_available()
-    file_path = [f for f in files_available if "tsp_105_1" in f][0]
+    file_path = [f for f in files_available if "tsp_5_1" in f][0]
     tsp_model = tsp_parser.parse_file(file_path)
     gpdp = ProxyClass.from_tsp_model_gpdp(tsp_model=tsp_model, compute_graph=True)
     print(gpdp.graph.get_nodes())
@@ -45,7 +45,7 @@ def test_tsp():
 @pytest.mark.skipif(not gurobi_available, reason="You need Gurobi to test this solver.")
 def test_tsp_simplified():
     files_available = tsp_parser.get_data_available()
-    file_path = [f for f in files_available if "tsp_105_1" in f][0]
+    file_path = [f for f in files_available if "tsp_5_1" in f][0]
     tsp_model = tsp_parser.parse_file(file_path)
     gpdp = ProxyClass.from_tsp_model_gpdp(tsp_model=tsp_model, compute_graph=True)
     gpdp = build_pruned_problem(gpdp, compute_graph=True)
@@ -66,7 +66,8 @@ def test_tsp_simplified():
 
 @pytest.mark.skipif(not gurobi_available, reason="You need Gurobi to test this solver.")
 def test_vrp():
-    file_path = vrp_parser.get_data_available()[3]
+    files_available = tsp_parser.get_data_available()
+    file_path = [f for f in files_available if "tsp_5_1" in f][0]
     vrp_model = vrp_parser.parse_file(file_path)
     gpdp = ProxyClass.from_vrp_model_to_gpdp(vrp_model=vrp_model, compute_graph=True)
     print(gpdp.graph.get_nodes())
@@ -86,7 +87,8 @@ def test_vrp():
 
 @pytest.mark.skipif(not gurobi_available, reason="You need Gurobi to test this solver.")
 def test_vrp_simplified():
-    file_path = vrp_parser.get_data_available()[3]
+    files_available = tsp_parser.get_data_available()
+    file_path = [f for f in files_available if "tsp_5_1" in f][0]
     vrp_model = vrp_parser.parse_file(file_path)
     gpdp = ProxyClass.from_vrp_model_to_gpdp(vrp_model=vrp_model, compute_graph=True)
     gpdp = build_pruned_problem(gpdp, compute_graph=True)
